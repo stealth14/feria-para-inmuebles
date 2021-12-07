bcb81054b60c9eba900163606739fedceb3676ff
Ronny Cajas <ronny1418@hotmail.com>
2021-12-06 20:21:04 -0500

    antd package, disabled SWC because babelrc file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+const withAntdLess = require("next-plugin-antd-less");
+
+module.exports = withAntdLess({
+  modifyVars: { "@primary-color": "523C3C" },
   future: {
     webpack5: false,
   },
   reactStrictMode: true,
-}
+});
