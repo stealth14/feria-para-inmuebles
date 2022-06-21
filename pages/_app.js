5f0266317dc0be677c911e2be9d7caff682cceb2
Ronny Cajas <ronny1418@hotmail.com>
2022-06-20 23:24:32 -0500

    .

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import GeneralNav from "@/components/GeneralNav";
 import { AuthProvider } from "@/lib/auth";
 import withLoader from "@/hocs/withLoader";
-
+//
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
