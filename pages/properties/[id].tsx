6e1ee4d389c195d4d9b7aa891d7d384df401d083
Ronny Cajas <ronny1418@hotmail.com>
2022-07-06 22:50:10 -0500

    type assertion

--- pages/properties/[id].tsx
+++ pages/properties/[id].tsx
@@ -82,7 +82,7 @@ export default function Page() {
               onClick={() => {
                 router.push({
                   pathname: "/properties/add",
-                  query: { ...property },
+                  query: { ...property } as any,
                 });
               }}
             />
