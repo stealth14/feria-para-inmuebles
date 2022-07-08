8fd19023d5da83a50a111a2af75569a3cb35364f
Ronny Cajas <ronny1418@hotmail.com>
2022-07-08 00:39:34 -0500

    reverts parsing of nextjs 'query'

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,6 +6,14 @@ export const numberOrAny = (value: any) => {
 export const parseQuery = (query: any) => {
   let parsedQuery = {};
   for (const entry in query) {
+    if (entry === "photos") {
+      //undo parse from array to string
+      if (typeof query[entry] === "string") {
+        parsedQuery = { ...parsedQuery, [entry]: [numberOrAny(query[entry])] };
+        continue;
+      }
+    }
+
     parsedQuery = { ...parsedQuery, [entry]: numberOrAny(query[entry]) };
   }
   return parsedQuery;
