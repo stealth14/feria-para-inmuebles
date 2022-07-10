54e74b52fcb34bd97f4b673744e88719960bbcd7
Ronny Cajas <ronny1418@hotmail.com>
2022-07-10 06:46:55 -0500

    'feature select' recycled in 'search' page

--- components/properties/FeatureSelect.tsx
+++ components/properties/FeatureSelect.tsx
@@ -4,21 +4,32 @@ import styles from "./FeatureSelect.module.scss";
 
 const featureOptions = [1, 2, 3, 4, 5];
 
-export default function FeatureSelect(props: { label: string; name: string }) {
-  const { label, name } = props;
+type align = "center" | "left";
+
+export default function FeatureSelect(props: {
+  label: string;
+  name: string;
+  align?: align;
+  required?: boolean;
+}) {
+  const { label, name, align = "left", required = true } = props;
   const [size, setSize] = React.useState("default");
 
   const handleSizeChange = (e: RadioChangeEvent) => {
     setSize(e.target.value);
   };
   return (
-    <div className={styles.container}>
+    <div
+      className={`${styles.container} ${align === "center" && styles.centered}`}
+    >
       <Form.Item
         label={label}
         name={name}
-        rules={[
-          { required: true, message: `Debe ingresar el número de ${label}` },
-        ]}
+        rules={
+          required && [
+            { required: true, message: `Debe ingresar el número de ${label}` },
+          ]
+        }
       >
         <Radio.Group name={name} value={size} onChange={handleSizeChange}>
           {featureOptions.map((featureOption) => {
--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,40 +1,82 @@
 import React from "react";
-import { Row, Col, Form } from "antd";
+import { Row, Col, Form, Input, Button } from "antd";
 import FeatureSelect from "@/components/properties/FeatureSelect";
 import styles from "./search.module.scss";
+
+interface Query {
+  [param: string]: string | number;
+}
 
 export default function Index() {
   const [form] = Form.useForm();
 
-  const onFinish = async (query) => {};
+  const onFinish = async (query: Query) => {
+    console.log("query:", query);
+  };
+
   return (
     <div className={styles.container}>
-      <Form
-        labelAlign="left"
-        labelCol={{ span: 5 }}
-        wrapperCol={{ span: 12 }}
-        layout="horizontal"
-        form={form}
-        onFinish={onFinish}
-      >
+      <Form layout={"vertical"} form={form} onFinish={onFinish}>
         <Row justify="center">
-          <Col span={24}>
-            <FeatureSelect name={"bathrooms"} label="Baños" />
+          <Col span={20}>
+            <Input.Group style={{ margin: "30px auto" }} compact>
+              <Form.Item noStyle label="Título" name="title">
+                <Input style={{ width: "calc(100% - 76px)" }} />
+              </Form.Item>
+              <Button htmlType="submit" type="primary">
+                Buscar
+              </Button>
+            </Input.Group>
           </Col>
-          <Col span={24}>
-            <FeatureSelect name={"area"} label="Area" />
+        </Row>
+        <Row justify="center">
+          <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+            <FeatureSelect
+              required={false}
+              align="center"
+              name={"bathrooms"}
+              label="Baños"
+            />
           </Col>
-          <Col span={24}>
-            <FeatureSelect name={"livingrooms"} label="Salas" />
+          <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+            <FeatureSelect
+              required={false}
+              align="center"
+              name={"area"}
+              label="Area"
+            />
           </Col>
-          <Col span={24}>
-            <FeatureSelect name={"bedrooms"} label="Dormitorios" />
+          <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+            <FeatureSelect
+              required={false}
+              align="center"
+              name={"livingrooms"}
+              label="Salas"
+            />
           </Col>
-          <Col span={24}>
-            <FeatureSelect name={"kitchens"} label="Cocinas" />
+          <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+            <FeatureSelect
+              required={false}
+              align="center"
+              name={"bedrooms"}
+              label="Dormitorios"
+            />
           </Col>
-          <Col span={24}>
-            <FeatureSelect name={"parkings"} label="Parqueaderos" />
+          <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+            <FeatureSelect
+              required={false}
+              align="center"
+              name={"kitchens"}
+              label="Cocinas"
+            />
+          </Col>
+          <Col xs={24} sm={12} md={12} lg={12} xl={12}>
+            <FeatureSelect
+              required={false}
+              align="center"
+              name={"parkings"}
+              label="Parqueaderos"
+            />
           </Col>
         </Row>
       </Form>
